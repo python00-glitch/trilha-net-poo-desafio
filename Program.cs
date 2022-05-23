1327933ec07bc4bab7b07656617ffc2abdae1394
Leonardo Buta <leonardo.lbuta@gmail.com>
2022-05-22 22:01:58 -0300

    Inicial

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using DesafioPOO.Models;
+
+// TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia nokia = new("98765432", "Nokia K1", "159753", 64);
+Iphone phone = new("98765432", "Iphone 11", "753951", 128);
+
+nokia.Ligar();
+phone.Ligar();
+
+phone.InstalarAplicativo("Whatsapp");
+nokia.InstalarAplicativo("Gmail");
